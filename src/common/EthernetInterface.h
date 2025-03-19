7808ee8671ef625a231cb6c23af09ade96200f30
spencerdee <spencerdee9406@gmail.com>
2025-03-19 06:57:13 -0400

    loopback

--- src/common/EthernetInterface.h
+++ src/common/EthernetInterface.h
@@ -11,7 +11,7 @@ class UDPClient {
     public:
         UDPClient() {
             if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-                printf("WSAStartup failed\n");
+                printf("[Client] WSAStartup failed\n");
             }
         };
 
@@ -25,7 +25,7 @@ class UDPClient {
         void SetupClient(uint32_t port, const char* ip) {
             clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
             if (clientSocket == INVALID_SOCKET) {
-                printf("Error creating client socket");
+                printf("[Client] Error creating client socket\n");
                 WSACleanup();
                 return;
             }
@@ -40,33 +40,36 @@ class UDPClient {
 
         void Send(const char* msg, size_t len) {
             if (!clientSetup) {
-                printf("Client not setup");
+                printf("[Client] Not setup\n");
                 return;
             }
             if (sendto(clientSocket, msg, len, 0, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-                printf("Error sending message: \"%s\"", msg);
+                printf("[Client] Error sending message: \"%s\"\n", msg);
                 closesocket(clientSocket);
                 WSACleanup();
                 return;
             }
         }
 
-        void Receive(char* buffer, size_t bufSize) {
+        size_t Receive(char* buffer, size_t bufSize) {
             if (!clientSetup) {
-                printf("Client not setup");
-                return;
+                printf("[Client] Not setup\n");
+                return 0;
             }
 
             int serverAddrLen = sizeof(serverAddr);
-            int bytesReceived = recvfrom(clientSocket, buffer, sizeof(buffer) - 1, 0, (sockaddr*)&serverAddr, &serverAddrLen);
+            int bytesReceived = recvfrom(clientSocket, buffer, sizeof(buffer), 0, (sockaddr*)&serverAddr, &serverAddrLen);
             if (bytesReceived == SOCKET_ERROR) {
-                printf("Error receiving message");
+                printf("[Client] Error receiving message\n");
             } else {
                 buffer[bytesReceived] = '\0';
-                printf("Received message: \"%s\"\n", buffer);
             }
+            return bytesReceived;
         }
 
+        bool IsSetup() {
+            return clientSetup;
+        }
 
     private:
         WSADATA wsaData;
@@ -79,7 +82,7 @@ class UDPServer {
     public:
         UDPServer() {
             if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-                printf("WSAStartup failed\n");
+                printf("[Server] WSAStartup failed\n");
             }
         };
 
@@ -94,7 +97,7 @@ class UDPServer {
             // Create socket
             serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
             if (serverSocket == INVALID_SOCKET) {
-                printf("Error creating server socket");
+                printf("[Server] Error creating server socket\n");
                 WSACleanup();
                 return;
             }
@@ -106,7 +109,7 @@ class UDPServer {
 
             // Bind socket
             if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-                printf("Error binding server socket");
+                printf("[Server] Error binding server socket\n");
                 closesocket(serverSocket);
                 WSACleanup();
                 return;
@@ -117,11 +120,12 @@ class UDPServer {
 
         void Send(const char* msg, size_t len) {
             if (!serverSetup) {
-                printf("Server not setup");
+                printf("[Server] Not setup\n");
                 return;
             }
             if (sendto(serverSocket, msg, len, 0, (sockaddr*)&clientAddr, sizeof(clientAddr)) == SOCKET_ERROR) {
-                printf("Error sending message: \"%s\"", msg);
+                printf("[Server] Error sending message: \"%s\"\n", msg);
+                printf("[Server] Error: %d\n", WSAGetLastError());
                 closesocket(serverSocket);
                 WSACleanup();
                 return;
@@ -130,21 +134,218 @@ class UDPServer {
 
         size_t Receive(char* buffer, size_t bufSize) {
             if (!serverSetup) {
-                printf("Server not setup");
+                printf("[Server] Not setup\n");
                 return 0;
             }
 
             int clientAddrLen = sizeof(clientAddr);
-            int bytesReceived = recvfrom(serverSocket, buffer, sizeof(buffer) - 1, 0, (sockaddr*)&clientAddr, &clientAddrLen);
+            int bytesReceived = recvfrom(serverSocket, buffer, sizeof(buffer), 0, (sockaddr*)&clientAddr, &clientAddrLen);
             if (bytesReceived == SOCKET_ERROR) {
-                printf("Error receiving message");
+                printf("[Server] Error receiving message\n");
+                printf("[Server] Error: %d\n", WSAGetLastError());
+                return 0;
             } else {
                 buffer[bytesReceived] = '\0';
-                if (bytesReceived > 0) {
-                    printf("Received message: \"%s\"\n", buffer);
-                }
             }
             return bytesReceived;
+        }
+
+        bool IsSetup() {
+            return serverSetup;
+        }
+
+    private:
+        WSADATA wsaData;
+        SOCKET serverSocket;
+        sockaddr_in clientAddr;
+        bool serverSetup = false;
+};
+
+class UDPMulticastClient {
+    public:
+        UDPMulticastClient() {
+            if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+                printf("[Client] WSAStartup failed\n");
+            }
+        };
+
+        ~UDPMulticastClient() {
+            if (clientSocket != INVALID_SOCKET) {
+                closesocket(clientSocket);
+            }
+            WSACleanup();
+        };
+
+        void SetupClient(uint32_t port, const char* ip) {
+            clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
+            if (clientSocket == INVALID_SOCKET) {
+                printf("[Client] Error creating client socket\n");
+                WSACleanup();
+                return;
+            }
+
+            // Configure server address
+            serverAddr.sin_family = AF_INET;
+            serverAddr.sin_port = htons(port);
+            inet_pton(AF_INET, ip, &serverAddr.sin_addr);
+
+            clientSetup = true;
+        }
+
+        void Send(const char* msg, size_t len) {
+            if (!clientSetup) {
+                printf("[Client] Not setup\n");
+                return;
+            }
+            if (sendto(clientSocket, msg, len, 0, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+                printf("[Client] Error sending message: \"%s\"\n", msg);
+                closesocket(clientSocket);
+                WSACleanup();
+                return;
+            }
+        }
+
+        size_t Receive(char* buffer, size_t bufSize) {
+            if (!clientSetup) {
+                printf("[Client] Not setup\n");
+                return 0;
+            }
+
+            int serverAddrLen = sizeof(serverAddr);
+            int bytesReceived = recvfrom(clientSocket, buffer, sizeof(buffer), 0, (sockaddr*)&serverAddr, &serverAddrLen);
+            if (bytesReceived == SOCKET_ERROR) {
+                printf("[Client] Error receiving message\n");
+                return 0;
+            } else {
+                buffer[bytesReceived] = '\0';
+            }
+            return bytesReceived;
+        }
+
+        bool IsSetup() {
+            return clientSetup;
+        }
+
+    private:
+        WSADATA wsaData;
+        SOCKET clientSocket;
+        sockaddr_in serverAddr;
+        bool clientSetup = false;
+};
+
+class UDPMulticastServer {
+    public:
+        UDPMulticastServer() {
+            if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+                printf("[Server] WSAStartup failed\n");
+            }
+        };
+
+        ~UDPMulticastServer() {
+            if (serverSocket != INVALID_SOCKET) {
+                closesocket(serverSocket);
+            }
+            WSACleanup();
+        };
+
+        void SetupServer(uint32_t port, const char* ip, bool internal = false) {
+            // Create socket
+            serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
+            if (serverSocket == INVALID_SOCKET) {
+                printf("[Server] Error creating server socket\n");
+                WSACleanup();
+                return;
+            }
+
+            // Allow reuse of port
+            bool yes = 1;
+            if ((setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&yes, sizeof(yes))) < 0)
+            {
+                printf("[Server] Error setting socket option: REUSEADDR\n");
+                closesocket(serverSocket);
+                return;
+            }
+
+            sockaddr_in serverAddr;
+            memset(&serverAddr, 0, sizeof(serverAddr));
+            serverAddr.sin_family = AF_INET;
+            serverAddr.sin_port = htons(port);
+            serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+            // Bind socket
+            if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+                printf("[Server] Error binding server socket\n");
+                closesocket(serverSocket);
+                WSACleanup();
+                return;
+            }
+            
+            ip_mreq mreq;
+            memset(&mreq, 0, sizeof(mreq));
+
+            if (internal) {
+                yes = true;
+                if (setsockopt(serverSocket, IPPROTO_IP, IP_MULTICAST_LOOP, (char*)&yes, sizeof(yes)) == SOCKET_ERROR) {
+                    printf("[Server] Error setting socket option: MULTICAST LOOP\n");
+                    printf("[Server] Error: %d\n", WSAGetLastError());
+                    closesocket(serverSocket);
+                    return;
+                }
+                inet_pton(AF_INET, "30.30.30.3", &mreq.imr_interface);
+            } else {
+                // mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+            }
+            // mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+
+            if (inet_pton(AF_INET, ip, &mreq.imr_multiaddr.s_addr) < 0) {
+                printf("[Server] Error setting multicast address\n");
+                closesocket(serverSocket);
+                return;
+            }
+        
+            if (setsockopt(serverSocket, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char*)&mreq, sizeof(mreq)) == SOCKET_ERROR) {
+                printf("[Server] Error setting socket option: MULTICAST MEMBERSHIP\n");
+                printf("[Server] Error: %d\n", WSAGetLastError());
+                closesocket(serverSocket);
+                return;
+            }
+
+            serverSetup = true;
+        }
+
+        void Send(const char* msg, size_t len) {
+            if (!serverSetup) {
+                printf("[Server] Not setup\n");
+                return;
+            }
+            if (sendto(serverSocket, msg, len, 0, (sockaddr*)&clientAddr, sizeof(clientAddr)) == SOCKET_ERROR) {
+                printf("[Server] Error sending message: \"%s\"\n", msg);
+                printf("[Server] Error: %d\n", WSAGetLastError());
+                closesocket(serverSocket);
+                WSACleanup();
+                return;
+            }
+        }
+
+        size_t Receive(char* buffer, size_t bufSize) {
+            if (!serverSetup) {
+                printf("[Server] Not setup\n");
+                return 0;
+            }
+
+            int clientAddrLen = sizeof(clientAddr);
+            int bytesReceived = recvfrom(serverSocket, buffer, sizeof(buffer), 0, (sockaddr*)&clientAddr, &clientAddrLen);
+            if (bytesReceived == SOCKET_ERROR) {
+                printf("[Server] Error receiving message\n");
+                printf("[Server] Error: %d\n", WSAGetLastError());
+            } else {
+                buffer[bytesReceived] = '\0';
+            }
+            return bytesReceived;
+        }
+
+        bool IsSetup() {
+            return serverSetup;
         }
 
     private:
--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,93 @@
 
 int main(int argc, char *argv[])
 {
-    UDPClient client;
-    client.SetupClient(2000, "127.0.0.1");
+    UDPMulticastClient client;
+    client.SetupClient(2002, "234.56.78.90");
 
-    UDPServer server;
-    server.SetupServer(2000);
+    UDPMulticastServer server;
+    server.SetupServer(2002, "234.56.78.90", true);
 
-    std::thread thread([&client] {
-        printf("Send Thread Started\n");
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        client.Send("Test", 5);
-        printf("Send Thread Ended\n");
-    });
+    if (!server.IsSetup()) {
+        printf("[Server] Setup failed\n");
+        return 1;
+    } else if (!client.IsSetup()) {
+        printf("[Client] Setup failed\n");
+        return 1;
+    }
 
     char buffer[1024];
     int rec = 0;
-    while (rec == 0) {
+
+    std::thread thread([&](){
+        char buffer[1024];
+        printf("[Client] Thread Started\n");
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        printf("[Client] Sending message\n");
+        client.Send("Message", 8);
+        while(!rec) {}
+        rec = 0;
+        while(!rec) {
+            rec = client.Receive(buffer, sizeof(buffer));
+            if (rec > 0) {
+                printf("[Client] Received message: \"%s\"\n", buffer);
+            }
+        }
+        printf("[Client] Thread Ended\n");
+    });
+
+    printf("[Server] Waiting for message\n");
+    while (!rec) {
         rec = server.Receive(buffer, sizeof(buffer));
+        if (rec > 0) {
+            printf("[Server] Received message: \"%s\"\n", buffer);
+            break;
+        }
     }
+    printf("[Server] Sending reply\n");
+    server.Send("Reply", 6);
 
     thread.join();
+    printf("[Server] Thread Ended\n");
     return 0;
+
+    // UDPClient client;
+    // client.SetupClient(2000, "127.0.0.1");
+
+    // UDPServer server;
+    // server.SetupServer(2000);
+
+    // char buffer[1024];
+    // int rec = 0;
+
+    // std::thread thread([&](){
+    //     char buffer[1024];
+    //     printf("[Client] Thread Started\n");
+    //     std::this_thread::sleep_for(std::chrono::seconds(2));
+    //     printf("[Client] Sending message\n");
+    //     client.Send("Message", 8);
+    //     while(!rec) {}
+    //     rec = 0;
+    //     while(!rec) {
+    //         rec = client.Receive(buffer, sizeof(buffer));
+    //         if (rec > 0) {
+    //             printf("[Client] Received message: \"%s\"\n", buffer);
+    //         }
+    //     }
+    //     printf("[Client] Thread Ended\n");
+    // });
+
+    // printf("[Server] Waiting for message\n");
+    // while (!rec) {
+    //     rec = server.Receive(buffer, sizeof(buffer));
+    //     if (rec > 0) {
+    //         printf("[Server] Received message: \"%s\"\n", buffer);
+    //         break;
+    //     }
+    // }
+    // printf("[Server] Sending reply\n");
+    // server.Send("Reply", 6);
+
+    // thread.join();
+    // printf("[Server] Thread Ended\n");
+    // return 0;
 }
